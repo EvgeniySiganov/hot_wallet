3161fd3889846657fb15de5e5f07766a861d523d
Evgeniy Siganov <EvgeniySiganov>
2025-11-12 19:47:13 +0300

    init

---
+++ src/main/java/ru/siganov/hot_wallet/HotWalletApplication.java
@@ -0,0 +1,13 @@
+package ru.siganov.hot_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotWalletApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HotWalletApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/siganov/hot_wallet/HotWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.siganov.hot_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotWalletApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

535f8fc71918b04947434c59415fa19a5d9b32ed
Evgeniy Siganov <EvgeniySiganov>
2025-12-12 19:10:02 +0300

    bug-fix count of barrier triggers and unnecessary sync

--- src/main/java/ru/siganov/hot_wallet/mydifficulttask/ComplexTaskExecutor.java
+++ src/main/java/ru/siganov/hot_wallet/mydifficulttask/ComplexTaskExecutor.java
@@ -11,25 +11,23 @@ import java.util.concurrent.Executors;
 public class ComplexTaskExecutor {
 
     ExecutorService executorService;
-    CyclicBarrier cyclicBarrier;
     List<Integer> tasks = Collections.synchronizedList(new ArrayList<>());
 
     public ComplexTaskExecutor(int threads) {
         executorService = Executors.newFixedThreadPool(threads);
-        cyclicBarrier = new CyclicBarrier(threads, () -> {
-            int sum = tasks.stream().mapToInt(Integer::intValue).sum();
-            System.out.println("sum: " + sum);
-        });
+
     }
 
     public void executeTasks(int countOfTasks) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(countOfTasks, () -> {
+            int sum = tasks.stream().mapToInt(Integer::intValue).sum();
+            System.out.println("sum: " + sum);
+        });
         for (int i = 0; i < countOfTasks; i++) {
             int taskId = i;
             executorService.submit(() -> {
                 ComplexTask task = new ComplexTask(taskId);
-                synchronized (tasks) {
-                    tasks.add(task.execute());
-                }
+                tasks.add(task.execute());
                 try {
                     cyclicBarrier.await();
                 } catch (InterruptedException | BrokenBarrierException e) {
